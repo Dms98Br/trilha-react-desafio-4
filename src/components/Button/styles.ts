18ce97201650ebed6d73a0e61f263444eaf19a74
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2024-09-15 10:42:45 -0300

    finalizando o desafio

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
 
+interface ButtonProps {
+    disabled?: boolean;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    children?: React.ReactNode;
+}
 
-export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+export const ButtonContainer = styled.button<ButtonProps>`
+  width: 100%;
+  height: 42px;
+  background-color: ${props => (props.disabled ? '#ccc' : '#81259D')};
+  color: #FFF;
+  border: 1px solid #81259D;
+  border-radius: 21px;
+  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  &:hover {
+    opacity: ${props => (props.disabled ? '1' : '0.6')};
+  }
+`;
